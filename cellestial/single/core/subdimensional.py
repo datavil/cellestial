d2c144c0b91e1fd74a3f9f7b84ea1f31ede60122
Zaf4 <zafermolbio@gmail.com>
2024-12-17 21:36:22 +0300

    improve dim plots

--- cellestial/single/core/subdimensional.py
+++ cellestial/single/core/subdimensional.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 
 from collections.abc import Iterable
-from typing import TYPE_CHECKING, Literal
+from typing import TYPE_CHECKING, Any, Literal
 
 from cellestial.single.core.dimensional import dimensional
 
@@ -15,27 +15,26 @@ def umap(
     key: Literal["leiden", "louvain"] | str = "leiden",
     *,
     size: float = 0.8,
-    point_shape: int = 16,
-    interactive: bool = False,  # used by interactive decorator
+    interactive: bool = False,
     cluster_name: str = "Cluster",
-    barcode_name: str = "Barcode",
+    barcode_name: str = "CellID",
     color_low: str = "#e6e6e6",
     color_high: str = "#377eb8",
     axis_type: Literal["axis", "arrow"] | None = None,
     arrow_length: float = 0.25,
-    arrow_size: float = 3,
+    arrow_size: float = 1,
     arrow_color: str = "#3f3f3f",
-    arrow_angle: float = 20,
+    arrow_angle: float = 10,
     show_tooltips: bool = True,
     add_tooltips: list[str] | tuple[str] | Iterable[str] | None = None,
     custom_tooltips: list[str] | tuple[str] | Iterable[str] | None = None,
+    **point_kwargs: dict[str, Any],
 ) -> PlotSpec:
     return dimensional(
         data=data,
         key=key,
         dimensions="umap",
         size=size,
-        point_shape=point_shape,
         interactive=interactive,
         cluster_name=cluster_name,
         barcode_name=barcode_name,
@@ -47,8 +46,9 @@ def umap(
         arrow_color=arrow_color,
         arrow_angle=arrow_angle,
         show_tooltips=show_tooltips,
-        add_tooltips=add_tooltips, 
+        add_tooltips=add_tooltips,
         custom_tooltips=custom_tooltips,
+        **point_kwargs,
     )
 
 
@@ -57,27 +57,26 @@ def tsne(
     key: Literal["leiden", "louvain"] | str = "leiden",
     *,
     size: float = 0.8,
-    point_shape: int = 16,
-    interactive: bool = False,  # used by interactive decorator
+    interactive: bool = False,
     cluster_name: str = "Cluster",
-    barcode_name: str = "Barcode",
+    barcode_name: str = "CellID",
     color_low: str = "#e6e6e6",
     color_high: str = "#377eb8",
     axis_type: Literal["axis", "arrow"] | None = None,
     arrow_length: float = 0.25,
-    arrow_size: float = 3,
+    arrow_size: float = 1,
     arrow_color: str = "#3f3f3f",
-    arrow_angle: float = 20,
+    arrow_angle: float = 10,
     show_tooltips: bool = True,
     add_tooltips: list[str] | tuple[str] | Iterable[str] | None = None,
     custom_tooltips: list[str] | tuple[str] | Iterable[str] | None = None,
+    **point_kwargs: dict[str, Any],
 ) -> PlotSpec:
     return dimensional(
         data=data,
         key=key,
         dimensions="tsne",
         size=size,
-        point_shape=point_shape,
         interactive=interactive,
         cluster_name=cluster_name,
         barcode_name=barcode_name,
@@ -89,8 +88,9 @@ def tsne(
         arrow_color=arrow_color,
         arrow_angle=arrow_angle,
         show_tooltips=show_tooltips,
-        add_tooltips=add_tooltips, 
+        add_tooltips=add_tooltips,
         custom_tooltips=custom_tooltips,
+        **point_kwargs,
     )
 
 
@@ -99,27 +99,26 @@ def pca(
     key: Literal["leiden", "louvain"] | str = "leiden",
     *,
     size: float = 0.8,
-    point_shape: int = 16,
-    interactive: bool = False,  # used by interactive decorator
+    interactive: bool = False,
     cluster_name: str = "Cluster",
-    barcode_name: str = "Barcode",
+    barcode_name: str = "CellID",
     color_low: str = "#e6e6e6",
     color_high: str = "#377eb8",
     axis_type: Literal["axis", "arrow"] | None = None,
     arrow_length: float = 0.25,
-    arrow_size: float = 3,
+    arrow_size: float = 1,
     arrow_color: str = "#3f3f3f",
-    arrow_angle: float = 20,
+    arrow_angle: float = 10,
     show_tooltips: bool = True,
     add_tooltips: list[str] | tuple[str] | Iterable[str] | None = None,
     custom_tooltips: list[str] | tuple[str] | Iterable[str] | None = None,
+    **point_kwargs: dict[str, Any],
 ) -> PlotSpec:
     return dimensional(
         data=data,
         key=key,
         dimensions="pca",
         size=size,
-        point_shape=point_shape,
         interactive=interactive,
         cluster_name=cluster_name,
         barcode_name=barcode_name,
@@ -131,6 +130,7 @@ def pca(
         arrow_color=arrow_color,
         arrow_angle=arrow_angle,
         show_tooltips=show_tooltips,
-        add_tooltips=add_tooltips, 
+        add_tooltips=add_tooltips,
         custom_tooltips=custom_tooltips,
+        **point_kwargs,
     )
