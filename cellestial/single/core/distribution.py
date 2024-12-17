ed5e4456e3d98d81731b840fe45ce7b8fd36bba2
Zaf4 <zafermolbio@gmail.com>
2024-12-17 22:24:34 +0300

    add grids args and additional kwargs fix no discrete x

--- cellestial/single/core/distribution.py
+++ cellestial/single/core/distribution.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 from collections.abc import Iterable
 from math import ceil
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
 
 # Core scverse libraries
 import polars as pl
@@ -51,6 +51,8 @@ def violin(
     add_tooltips: list[str] | tuple[str] | Iterable[str] | None = None,
     custom_tooltips: list[str] | tuple[str] | Iterable[str] | None = None,
     interactive: bool = False,
+    point_kwargs: dict[str, Any] | None = None,
+    **violin_kwargs: dict[str, Any],
 ) -> PlotSpec:
     # check if data is an AnnData object
     if not isinstance(data, sc.AnnData):
@@ -62,6 +64,31 @@ def violin(
     if key not in frame.columns:
         msg = f"key must be a column in the AnnData object, but {key} is not in the columns"
         raise KeyError(msg)
+
+    # handle violin_kwargs
+    if violin_kwargs:
+        if "fill" in violin_kwargs:
+            violin_fill = violin_kwargs.get("fill")
+        if "color" in violin_kwargs:
+            violin_color = violin_kwargs.get("color")
+        if "trim" in violin_kwargs:
+            trim = violin_kwargs.get("trim")
+        if "tooltips" in violin_kwargs:
+            msg = "violin tooltips are non-customizable by `violin_kwargs`"
+            raise KeyError(msg)
+    # handle point_kwargs
+    if point_kwargs is None:
+        point_kwargs = {}
+    else:
+        if "color" in point_kwargs:
+            point_color = point_kwargs.get("color")
+        if "alpha" in point_kwargs:
+            point_alpha = point_kwargs.get("alpha")
+        if "size" in point_kwargs:
+            point_size = point_kwargs.get("size")
+        if "tooltips" in point_kwargs:
+            msg = "use tooltips args within the function instead of adding `'tooltips' : 'value'` to `point_kwargs`\n"
+            raise KeyError(msg)
 
     # handle tooltips
     base_tooltips = ["CellID", key]
@@ -87,7 +114,7 @@ def violin(
         ggplot(data=frame)
         + geom_violin(
             data=frame,
-            mapping=aes(y=key, color=color, fill=fill),
+            mapping=aes(x=fill,y=key, color=color, fill=fill),
             fill=violin_fill,
             color=violin_color,
             trim=trim,
@@ -99,7 +126,7 @@ def violin(
     if show_points:
         vln += geom_jitter(
             data=frame,
-            mapping=aes(y=key),
+            mapping=aes(x=fill,y=key),
             color=point_color,
             alpha=point_alpha,
             size=point_size,
@@ -144,7 +171,19 @@ def violins(
     layers: list[str] | tuple[str] | Iterable[str] | None = None,
     interactive: bool = False,
     multi_panel: bool = True,
-    **grid_kwargs,
+    # grid args
+    ncol: int | None = None,
+    sharex: str | None = None,
+    sharey: str | None = None,
+    widths: list | None = None,
+    heights: list | None = None,
+    hspace: float | None = None,
+    vspace: float | None = None,
+    fit: bool | None = None,
+    align: bool | None = None,
+    # other kwargs
+    point_kwargs: dict[str, Any] | None = None,
+    **violin_kwargs: dict[str, Any],
 ):
     if multi_panel:  # standard grid plotting
         plots = []
@@ -165,6 +204,8 @@ def violins(
                 add_tooltips=add_tooltips,
                 custom_tooltips=custom_tooltips,
                 interactive=interactive,
+                point_kwargs=point_kwargs,
+                **violin_kwargs,
             )
             # handle the layers
             if layers is not None:
@@ -173,7 +214,18 @@ def violins(
 
             plots.append(vln)
 
-        vlns = gggrid(plots, ncol=grid_kwargs.get("ncol"))
+        vlns = gggrid(
+            plots,
+            ncol=ncol,
+            sharex=sharex,
+            sharey=sharey,
+            widths=widths,
+            heights=heights,
+            hspace=hspace,
+            vspace=vspace,
+            fit=fit,
+            align=align,
+        )
 
     else:  # unpivot the data so that it can be plotted in a single (combined) panel
         frame = pl.from_pandas(data.obs[keys], include_index=True).rename({"None": "CellID"})
@@ -208,13 +260,13 @@ def boxplot(
     point_color: str = "#1f1f1f",
     point_alpha: float = 0.7,
     point_size: float = 0.5,
-    trim: bool = False,
     show_tooltips: bool = True,
     show_points: bool = False,
     add_tooltips: list[str] | tuple[str] | Iterable[str] | None = None,
     custom_tooltips: list[str] | tuple[str] | Iterable[str] | None = None,
     interactive: bool = False,
-    boxplot_kwargs: dict | None = None,
+    point_kwargs: dict[str, Any] | None = None,
+    **boxplot_kwargs: dict[str, Any],
 ) -> PlotSpec:
     # check if data is an AnnData object
     if not isinstance(data, sc.AnnData):
@@ -227,6 +279,28 @@ def boxplot(
         msg = f"key must be a column in the AnnData object, but {key} is not in the columns"
         raise KeyError(msg)
 
+    # handle violin_kwargs
+    if boxplot_kwargs:
+        if "fill" in boxplot_kwargs:
+            boxplot_fill = boxplot_kwargs.get("fill")
+        if "color" in boxplot_kwargs:
+            boxplot_color = boxplot_kwargs.get("color")
+        if "tooltips" in boxplot_kwargs:
+            msg = "violin tooltips are non-customizable by `boxplot_kwargs`"
+            raise KeyError(msg)
+    # handle point_kwargs
+    if point_kwargs is None:
+        point_kwargs = {}
+    else:
+        if "color" in point_kwargs:
+            point_color = point_kwargs.get("color")
+        if "alpha" in point_kwargs:
+            point_alpha = point_kwargs.get("alpha")
+        if "size" in point_kwargs:
+            point_size = point_kwargs.get("size")
+        if "tooltips" in point_kwargs:
+            msg = "use tooltips args within the function instead of adding `'tooltips' : 'value'` to `point_kwargs`\n"
+            raise KeyError(msg)
     # handle tooltips
     base_tooltips = ["CellID", key]
     if not show_tooltips:
@@ -259,7 +333,6 @@ def boxplot(
             mapping=aes(y=key, color=color, fill=fill),
             fill=boxplot_fill,
             color=boxplot_color,
-            trim=trim,
             tooltips=layer_tooltips(boxplot_tooltips),
             **boxplot_kwargs,
         )
@@ -314,7 +387,19 @@ def boxplots(
     layers: list[str] | tuple[str] | Iterable[str] | None = None,
     interactive: bool = False,
     multi_panel: bool = True,
-    **grid_kwargs,
+    # grid args
+    ncol: int | None = None,
+    sharex: str | None = None,
+    sharey: str | None = None,
+    widths: list | None = None,
+    heights: list | None = None,
+    hspace: float | None = None,
+    vspace: float | None = None,
+    fit: bool | None = None,
+    align: bool | None = None,
+    # other kwargs
+    point_kwargs: dict[str, Any] | None = None,
+    **boxplot_kwargs: dict[str, Any],
 ):
     if multi_panel:  # standard grid plotting
         plots = []
@@ -335,6 +420,8 @@ def boxplots(
                 add_tooltips=add_tooltips,
                 custom_tooltips=custom_tooltips,
                 interactive=interactive,
+                point_kwargs=point_kwargs,
+                **boxplot_kwargs,
             )
             # handle the layers
             if layers is not None:
@@ -343,7 +430,18 @@ def boxplots(
 
             plots.append(bxplt)
 
-        bxplts = gggrid(plots, ncol=grid_kwargs.get("ncol"))
+        bxplts = gggrid(
+            plots,
+            ncol=ncol,
+            sharex=sharex,
+            sharey=sharey,
+            widths=widths,
+            heights=heights,
+            hspace=hspace,
+            vspace=vspace,
+            fit=fit,
+            align=align,
+        )
 
     else:  # unpivot the data so that it can be plotted in a single (combined) panel
         frame = pl.from_pandas(data.obs[keys], include_index=True).rename({"None": "CellID"})
