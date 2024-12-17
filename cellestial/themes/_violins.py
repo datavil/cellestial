41e0f09804ab8a535e7ca7adc95d3f21ad1e9ba8
Zaf4 <zafermolbio@gmail.com>
2024-12-17 22:24:34 +0300

    add grids args and additional kwargs fix no discrete x

--- cellestial/themes/_violins.py
+++ cellestial/themes/_violins.py
@@ -3,9 +3,9 @@ from lets_plot import element_text, ggsize, scale_fill_hue, theme, theme_classic
 _THEME_VIOLIN = (
     theme_classic()
     + theme(
-        text=element_text(family="Arial", color="#3f3f3f"),
-        title=element_text(family="Arial", color="#3f3f3f"),
-        legend_title=element_text(family="Arial", color="#3f3f3f", face="Bold"),
+        text=element_text(family="Arial", color="#1f1f1f"),
+        title=element_text(family="Arial", color="#1f1f1f"),
+        legend_title=element_text(family="Arial", color="#1f1f1f", face="Bold"),
     )
     + scale_fill_hue()
     + ggsize(400, 400)
@@ -14,9 +14,9 @@ _THEME_VIOLIN = (
 _THEME_BOXPLOT = (
     theme_classic()
     + theme(
-        text=element_text(family="Arial", color="#3f3f3f"),
-        title=element_text(family="Arial", color="#3f3f3f"),
-        legend_title=element_text(family="Arial", color="#3f3f3f", face="Bold"),
+        text=element_text(family="Arial", color="#1f1f1f"),
+        title=element_text(family="Arial", color="#1f1f1f"),
+        legend_title=element_text(family="Arial", color="#1f1f1f", face="Bold"),
     )
     + scale_fill_hue()
     + ggsize(400, 400)
