c3b8ec12cd39a0956adc9a9dfd6fa9e0651ae21d
Zaf4 <zafermolbio@gmail.com>
2025-01-14 22:32:33 +0300

    move colors into module level init

--- cellestial/__init__.py
+++ cellestial/__init__.py
@@ -19,6 +19,14 @@ from cellestial.single import (
 )
 from cellestial.util import _add_arrow_axis, interactive
 
+# Hand-picked colors for cellestial
+TEAL = "#219B9D"
+RED = "#D2042D"
+CHERRY = "#AF1740"
+BLUE = "#377EB8"
+LIGHT_GRAY = "#E6E6E6"
+
+
 __all__ = [
     "interactive",
     "_add_arrow_axis",
@@ -39,4 +47,9 @@ __all__ = [
     "boxplots",
     "dotplot",
     "versions",
+    "TEAL",
+    "RED",
+    "CHERRY",
+    "BLUE",
+    "LIGHT_GRAY",
 ]
