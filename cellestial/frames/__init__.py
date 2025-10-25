27d82b7d90fff1557fefd1e9eb726366d3f9f073
Zaf4 <zafermolbio@gmail.com>
2025-10-25 19:09:00 +0300

    fixes

--- cellestial/frames/__init__.py
+++ cellestial/frames/__init__.py
@@ -1,4 +1,5 @@
 from cellestial.frames.construct import _axis_data, _construct_cell_frame, _construct_var_frame
+from cellestial.frames.build import anndata_observations_frame, anndata_variables_frame
 
 __all__ = [
     "_axis_data",
