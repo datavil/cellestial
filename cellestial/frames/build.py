4cd683ea66874f075ef584975656b93861e77b35
Zaf4 <zafermolbio@gmail.com>
2025-10-25 20:45:56 +0300

    add build frame and fix categorical interger problem

--- cellestial/frames/build.py
+++ cellestial/frames/build.py
@@ -1,15 +1,16 @@
 from __future__ import annotations
 
+import contextlib
 from collections.abc import Sequence
-from typing import Iterable
+from typing import Literal
 
 import polars as pl
 from anndata import AnnData
 
-from cellestial.util.errors import ConflictingKeysError, KeyNotFoundError
+from cellestial.util.errors import KeyNotFoundError
 
 
-def _add_variable_columns(data: AnnData,frame: pl.DataFrame, keys: str | Sequence[str]) -> pl.DataFrame:
+def _add_anndata_variable_columns(data: AnnData,frame: pl.DataFrame, keys: str | Sequence[str]) -> pl.DataFrame:
     """Add variable keys to the DataFrame."""
     if isinstance(keys, str):
         keys = [keys]
@@ -25,7 +26,7 @@ def _add_variable_columns(data: AnnData,frame: pl.DataFrame, keys: str | Sequenc
             )
         else:
             msg = f"Key `{key}` not found in data."
-            raise ValueError(msg)
+            raise KeyNotFoundError(msg)
 
     return frame
 
@@ -44,12 +45,12 @@ def anndata_observations_frame(
     ----------
     data : AnnData
         The AnnData object containing the observations.
-    variable_keys : str or Sequence[str] or None
+    variable_keys : str | Sequence[str] | None
         Variable keys to add to the DataFrame. If None, no additional keys are added.
     observations_name : str, optional
-        The name of the observations column, by default "barcode".
+        The name of the observations column, default is "barcode".
     include_dimensions : bool, optional
-        Whether to include dimensions from `obsm` in the DataFrame, by default False.
+        Whether to include dimensions from `obsm` in the DataFrame, default is False.
 
     Returns
     -------
@@ -66,6 +67,11 @@ def anndata_observations_frame(
     frame = frame.with_columns(pl.Series(observations_name, data.obs_names))
     # PART 2: ADD AnnData.obs
     for key in data.obs.columns:
+        # handle categorical integer data
+        if data.obs[key].dtype == "category":
+            with contextlib.suppress(ValueError):
+                data.obs[key] = data.obs[key].astype("int32")
+        # add the columns
         frame = frame.with_columns(pl.Series(key, data.obs[key]))
     # PART 4: ADD dimensions if needed
     if include_dimensions:
@@ -76,7 +82,7 @@ def anndata_observations_frame(
 
     # PART 5: ADD keys if provided
     if variable_keys is not None:
-        frame = _add_variable_columns(frame, variable_keys)
+        frame = _add_anndata_variable_columns(frame, variable_keys)
 
     return frame
 
@@ -115,6 +121,11 @@ def anndata_variables_frame(
 
     # PART 3: ADD AnnData.var
     for key in data.var.columns:
+        # handle categorical integer data
+        if data.var[key].dtype == "category":
+            with contextlib.suppress(ValueError):
+                data.var[key] = data.var[key].astype("int32")
+        # add the columns
         frame = frame.with_columns(pl.Series(key, data.var[key]))
 
     # PART 4: ADD dimensions if needed
@@ -123,5 +134,66 @@ def anndata_variables_frame(
             col_count = data.varm[X].shape[1] # Number of dimensions (columns)
             for col in range(col_count):
                 frame = frame.with_columns(pl.Series(f"{X}_{col+1}", data.varm[X][:, col]))
+
+    return frame
+
+
+
+def build_frame(
+    data: AnnData,
+    *,
+    variable_keys: str | Sequence[str] | None = None,
+    axis: Literal[0,1] | None = None,
+    observations_name: str = "barcode",
+    variables_name: str = "variable",
+    include_dimensions: bool = False,
+) -> pl.DataFrame:
+    """
+    Build a DataFrame from an AnnData object.
+
+    Parameters
+    ----------
+    data : AnnData
+        The AnnData object containing the variables.
+    variable_keys : str | Sequence[str] | None
+        Variable keys to add to the DataFrame. If None, no additional keys are added.
+    axis : Literal[0,1] | None
+        The axis to build the frame for. 0 for observations, 1 for variables.
+    observations_name : str
+        The name of the observations column, default is "barcode".
+    variables_name : str
+        Name for the variables index column, default is 'variable'
+    include_dimensions : bool
+        Whether to include dimensionality reductions fields.
+
+    Returns
+    -------
+    pl.DataFrame
+        A DataFrame containing the variables.
+    """
+    if isinstance(data, AnnData):
+        # infer the axis if not provided
+        if axis is None and variable_keys is not None:
+            axis = 0
+
+        if axis == 0:
+            frame = anndata_observations_frame(
+                data,
+                variable_keys=variable_keys,
+                observations_name=observations_name,
+                include_dimensions=include_dimensions,
+            )
+        elif axis == 1:
+            frame =  anndata_variables_frame(
+                data,
+                variables_name=variables_name,
+                include_dimensions=include_dimensions,
+            )
+        elif axis is None:
+            msg = "`axis` parameter must be specified, 0 for observations, 1 for variables."
+            raise ValueError(msg)
+    else:
+        msg = f"Unsupported data type: `{type(data)}`"
+        raise TypeError(msg)
 
     return frame
