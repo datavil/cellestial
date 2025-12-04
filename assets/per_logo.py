a1627f6bbd9fbb16405aae5cf9a9062db56404fb
Zaf4 <zafermolbio@gmail.com>
2025-12-04 16:48:34 +0300

    test with positron

--- assets/per_logo.py
+++ assets/per_logo.py
@@ -1,17 +1,20 @@
+from lets_plot.plot.theme_set import theme_minimal2
 import scanpy as sc
 from lets_plot import *
 
 import cellestial as cl
 
-data = sc.read("../data/pbmc3k_pped.h5ad")
+data = sc.read("data/pbmc3k_pped.h5ad")
 
 
-plot = cl.umap(data, axis_type=None, size=0.12)
+plot = cl.umap(data, axis_type=None, size=0.12, key="leiden")
 plot = plot + theme_void() + ggsize(200, 200)
 plot = plot + theme(legend_position="none")
 plot = plot + theme(
     legend_background=element_blank(),
     plot_background=element_blank(),
 ) + scale_color_hue()
+plot + theme_void()
+
 
 plot.to_pdf("test/per_logo.pdf")
