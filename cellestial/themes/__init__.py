487f078dd140416c1e98f946159c5417644fb005
Zaf4 <zafermolbio@gmail.com>
2026-01-26 07:06:19 +0300

    dotplot almost done

--- cellestial/themes/__init__.py
+++ cellestial/themes/__init__.py
@@ -1,4 +1,4 @@
-from cellestial.themes._scatters import _THEME_DIMENSION, _THEME_SCATTER
+from cellestial.themes._scatters import _THEME_DIMENSION, _THEME_DOTPLOT, _THEME_SCATTER
 from cellestial.themes._violins import _THEME_DIST
 
-__all__ = ["_THEME_DIMENSION", "_THEME_DIST", "_THEME_SCATTER"]
+__all__ = ["_THEME_DIMENSION", "_THEME_DIST", "_THEME_DOTPLOT", "_THEME_SCATTER"]
